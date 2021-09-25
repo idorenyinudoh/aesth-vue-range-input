cdd2fa4150b25a6bb6b22397b5a2c7c77502e284
idorenyinudoh <idorenyinudoh10@outlook.com>
2021-09-25 19:07:16 +0100

    delete default vue files and add mine

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,15 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <base-range></base-range>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import BaseRange from "./components/BaseRange.vue";
 
 export default {
-  name: "App",
   components: {
-    HelloWorld,
+    BaseRange,
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
